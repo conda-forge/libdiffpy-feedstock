c3bd27e93ecc4a2d5673254aa01a0e1c75226823
Pavol Juhas <pavol.juhas@gmail.com>
2019-03-10 17:45:21 -0400

    Sync with conda-recipe in diffpy/libdiffpy v1.4.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export CPATH="${PREFIX}/include:${CPATH}"
-export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
-
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
-# apply customizations in sconscript.local
+# drop linker flags that spuriously remove linkage with libgslcblas
+LDFLAGS="${LDFLAGS/-Wl,-dead_strip_dylibs/}"
+
+# Apply sconscript.local customizations.
 cp ${RECIPE_DIR}/sconscript.local ./
 
-# Build and install the library
+# Build and install the library.
 scons -j $MYNCPU lib install prefix=$PREFIX
