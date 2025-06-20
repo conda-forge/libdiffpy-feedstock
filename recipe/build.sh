2bd140bbbfc8c8e4e82c2e523b60b479a4f00a25
Tieqiong Zhang <tz2600@columbia.edu>
2025-06-20 16:54:39 -0500

    still linux prefix

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,10 +6,6 @@ MYNCPU=$(( CPU_COUNT > 8 ? 8 : CPU_COUNT ))
 # drop linker flags that spuriously remove linkage with libgslcblas
 LDFLAGS="${LDFLAGS/-Wl,-dead_strip_dylibs/}"
 
-# satisfy linux build shell
-LDFLAGS="${LDFLAGS//\$PREFIX/${PREFIX}}"
-export LDFLAGS
-
 # use macos SDK
 if [[ $build_platform == osx-64 ]]; then
   export CXXFLAGS="${CXXFLAGS} -isysroot ${CONDA_BUILD_SYSROOT}"
