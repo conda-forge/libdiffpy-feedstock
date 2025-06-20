c7ce9c9d2845a34d963f0e5711abf8243433b38e
Tieqiong Zhang <tz2600@columbia.edu>
2025-06-20 17:11:50 -0500

    debug message

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,12 @@ MYNCPU=$(( CPU_COUNT > 8 ? 8 : CPU_COUNT ))
 # drop linker flags that spuriously remove linkage with libgslcblas
 LDFLAGS="${LDFLAGS/-Wl,-dead_strip_dylibs/}"
 
+#debug
+echo "===== PREFIX is: $PREFIX ====="
+echo "===== Listing $PREFIX/lib ====="
+ls -al "$PREFIX/lib" || true
+echo "================================"
+
 # use macos SDK
 if [[ $build_platform == osx-64 ]]; then
   export CXXFLAGS="${CXXFLAGS} -isysroot ${CONDA_BUILD_SYSROOT}"
